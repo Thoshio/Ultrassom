0475fa39ef62cb270a2860a9c58bf9734cf961cc
Thoshio Onuki Neto <thoshio@terra.com.br>
2026-05-29 11:08:35 -0300

    criando projeto

---
+++ src/main.c
@@ -0,0 +1,15 @@
+#include <zephyr/kernel.h>             // Funções básicas do Zephyr (ex: k_msleep, k_thread, etc.)
+#include <zephyr/device.h>             // API para obter e utilizar dispositivos do sistema
+#include <zephyr/drivers/gpio.h>       // API para controle de pinos de entrada/saída (GPIO)
+#include <zephyr/console/console.h>    // API para entrada de informações pelo console/serial monitor
+
+int main(void)
+{
+
+    for (;;)
+    {
+
+    }
+
+    return 0;
+}
